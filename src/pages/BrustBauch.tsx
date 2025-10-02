34b27fb459675e1efeaf028e9bd63d0b824df28d
derma-light <ralf.kreppein@googlemail.com>
2025-10-02 05:26:00 +0200

    Updated index.html

--- src/pages/BrustBauch.tsx
+++ src/pages/BrustBauch.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import SEO from '../components/SEO';
 import Header from '../components/Header';
 import DropdownScript from '../components/DropdownScript';
-import IntimbereichMaennerNeuHero from '../components/IntimbereichMaennerNeuHero';
-import IntimbereichMaennerNeuBannerIntro from '../components/IntimbereichMaennerNeuBannerIntro';
-import IntimbereichMaennerNeuBenefits from '../components/IntimbereichMaennerNeuBenefits';
-import IntimbereichMaennerNeuProcess from '../components/IntimbereichMaennerNeuProcess';
-import IntimbereichMaennerNeuFAQ from '../components/IntimbereichMaennerNeuFAQ';
-import IntimbereichMaennerNeuPricing from '../components/IntimbereichMaennerNeuPricing';
+import BrustHero from '../components/BrustHero';
+import BrustBenefits from '../components/BrustBenefits';
+import BrustProcess from '../components/BrustProcess';
+import BrustFAQ from '../components/BrustFAQ';
 import Breadcrumbs from '../components/Breadcrumbs';
 import InternalLinkingWidget from '../components/InternalLinkingWidget';
 import TreatmentNavigation from '../components/TreatmentNavigation';
@@ -50,15 +48,13 @@ const BrustBauch = () => {
       <Header />
       <DropdownScript />
       <Breadcrumbs />
-      <IntimbereichMaennerNeuHero />
-      <IntimbereichMaennerNeuBannerIntro />
-      <IntimbereichMaennerNeuBenefits />
-      <IntimbereichMaennerNeuProcess />
+      <BrustHero />
+      <BrustBenefits />
+      <BrustProcess />
       <div className="container mx-auto px-4 lg:px-6 py-12">
         <InternalLinkingWidget currentPage="/maenner/brust-bauch" />
       </div>
-      <IntimbereichMaennerNeuFAQ />
-      <IntimbereichMaennerNeuPricing />
+      <BrustFAQ />
       <div className="container mx-auto px-4 lg:px-6">
         <TreatmentNavigation currentPath="/maenner/brust-bauch" />
       </div>
